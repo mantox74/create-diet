26bed50e739e212be3890a4f973a2b7a3df3e8a0
Massimiliano Mantovani <massimiliano.mantovani@cgm.com>
2023-03-03 12:33:55 +0100

    create angular project

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from '.c:/Users/massimiliano.mantova/Documents/Privato/progetti/create-diet/home/home.component';
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+      HomeComponent
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
