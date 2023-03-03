1554a82390155650eabe814f03801cad3388b742
Massimiliano Mantovani <massimiliano.mantovani@cgm.com>
2023-03-03 15:05:01 +0100

    update animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from '.c:/Users/massimiliano.mantova/Documents/Privato/progetti/create-diet/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [	
-    AppComponent,
-      HomeComponent
+    AppComponent
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
